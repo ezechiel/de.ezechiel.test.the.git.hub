338ffa1aa579e13f40bb0430edaa673ea9a7cdb4
Heer der Ringe <Heer der Ringe@stefan>
2012-09-06 20:25:46 +0200

    added and more

--- de.test.the.git/src/de/test/the/git/HelloWorld.java
+++ de.test.the.git/src/de/test/the/git/HelloWorld.java
@@ -3,7 +3,7 @@ package de.test.the.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World and so on");
 	}
 	
 }
